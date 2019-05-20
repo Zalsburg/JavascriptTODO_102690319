8af18a5fd0e6e3b2f19539174dcf0d7de7d90940
Zali <48194062+Zalsburg@users.noreply.github.com>
2019-05-20 16:55:48 +1000

    put todos in categories

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-var addButton = document.getElementById("addButton");
-var item = document.getElementById("item");
-var list = document.getElementById("list");
-var clearButton = document.getElementById("clearButton");
+let addButton = document.getElementById("addButton");
+let item = document.getElementById("item");
+let list = document.getElementById("list");
+let clearButton = document.getElementById("clearButton");
+let categories = document.getElementById("categories");
 
 addButton.onclick = addButtonClicked;
 clearButton.onclick = clearButtonClicked;
 
 function addButtonClicked() {
-    var listItem = item.value;
+    let listItem = item.value;
     console.log(listItem);
-    var itemInput = document.createElement("div");
+    let itemInput = document.createElement("div");
+    itemInput.setAttribute("class", "itemContainer");
     itemInput.appendChild(document.createTextNode(listItem));
+    
+    let taskList = document.getElementById(categories.value);
     taskList.appendChild(itemInput);
 
     document.getElementById("item").value = "";
 
-    var checkbox = document.createElement("input");
+    let checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
     checkbox.setAttribute("class", "checkbox");
     itemInput.appendChild(checkbox);
 
-    var x = document.createElement("button");
+    let x = document.createElement("button");
     x.setAttribute("class", "removeItem");
     x.appendChild(document.createTextNode("X"));
     itemInput.appendChild(x);
@@ -29,11 +33,15 @@ function addButtonClicked() {
 }
 
 function clearButtonClicked() {
-    var clearAllItems = document.getElementById("taskList");
-    clearAllItems.parentNode.removeChild(clearAllItems);
+    let clearAllItems = document.getElementById("taskList");
+    clearAllItems.innerHTML = "Shopping";
+
+    clearAllItems = document.getElementById("uniList");
+    clearAllItems.innerHTML = "Uni To Do";
+
+    clearAllItems = document.getElementById("actList");
+    clearAllItems.innerHTML = "Unspeakable Acts";
 }
-
-
 
 function removeItemClicked(e) {
     e.target.parentNode.remove();
