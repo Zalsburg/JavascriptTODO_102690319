6f7900f78259f9547d9880053a2d3b69e7c11e28
Zali <48194062+Zalsburg@users.noreply.github.com>
2019-05-20 14:08:15 +1000

    user can clear all tasks

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var addButton = document.getElementById("addButton");
 var item = document.getElementById("item");
 var list = document.getElementById("list");
+var clearButton = document.getElementById("clearButton");
 
 addButton.onclick = addButtonClicked;
+clearButton.onclick = clearButtonClicked;
 
 function addButtonClicked() {
     var listItem = item.value;
     console.log(listItem);
     var itemInput = document.createElement("div");
+    //itemInput.setAttribute("id", "task");
     itemInput.appendChild(document.createTextNode(listItem));
-    list.appendChild(itemInput);
+    taskList.appendChild(itemInput);
 
     document.getElementById("item").value = "";
 }
 
+function clearButtonClicked() {
+    var clearItem = document.getElementById("taskList");
+    clearItem.parentNode.removeChild(clearItem);
+}
